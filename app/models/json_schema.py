c186c513ff96df763a6b773112303edae0ff9c9c
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-11 17:30:46 +0200

    read the json schema from a file

--- app/models/json_schema.py
+++ app/models/json_schema.py
@@ -1,33 +1,12 @@
+import os
 from jsonschema import validate, ValidationError
+from json import load
+from flask import current_app
 
-expense_schema = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "ExpenseObject",
-    "description": "timetmap regex from https://www.myintervals.com/blog/2009/05/20/iso-8601-date-validation-that-doesnt-suck/ and modified to require tz",
-    "type": "object",
-    "properties": {
-        "id": {"type": ["number", "null"]},
-        "name": {"type": "string"},
-        "amount": {"type": "number"},
-        "tags": {"type": "array", "items": {"type": "string"}},
-        "currency": {"type": "string", "pattern": "^[A-Z]{3}$"},
-        "timestamp_utc": {
-            "type": "string",
-            "pattern": "^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?))?)?$"
-        },
-        "timestamp_utc_created": {
-            "type": "string",
-            "pattern": "^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?))?)?$"
-        },
-        "timestamp_utc_updated": {
-            "type": "string",
-            "pattern": "^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?))?)?$"
-        }
-    },
-    "required": ["id", "name", "amount", "tags", "currency", "timestamp_utc", "timestamp_utc_created",
-                 "timestamp_utc_updated"]
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-}
+with open(dir_path + "/../para-common/expense_json_schema.json") as file:  # todo better way to reference the file
+    expense_schema = load(file)
 
 if __name__ == "__main__":
     valid = {
