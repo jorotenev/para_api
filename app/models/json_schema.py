0b8daade1e2f8d82fb654ec47acbbe167d0d1e7a
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-18 22:44:00 +0200

    extract the valid property names of an expense into a variable.

--- app/models/json_schema.py
+++ app/models/json_schema.py
@@ -7,7 +7,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 
 with open(dir_path + "/../para-common/expense_json_schema.json") as file:  # todo better way to reference the file
     expense_schema = load(file)
-
+    expense_properties = list(expense_schema['properties'].keys())
 if __name__ == "__main__":
     valid = {
         "id": "1",
