fc15aeb18630aca63b97bc9142fb297ac17b4241
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-18 14:20:52 +0200

    update the utc_now method to return a compliant format str (with a forced Z at the end)

--- app/helpers/time.py
+++ app/helpers/time.py
@@ -1,5 +1,12 @@
 from datetime import datetime, timezone
 
 
-def utc_now():
-    return datetime.now(timezone.utc).isoformat()
+def utc_now_str():
+    return ensure_ts_str_ends_with_z(datetime.now(timezone.utc).isoformat())
+
+
+def ensure_ts_str_ends_with_z(ts_string: str):
+    suffix = '+00:00'
+    if ts_string.endswith(suffix):
+        return ts_string.replace(suffix, 'Z')
+    return ts_string
