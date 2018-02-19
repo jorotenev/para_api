119a3a22da0bbf705d401935e42a89f9ce75a7b3
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-19 14:39:20 +0200

    add a comment about the format of utc str

--- app/helpers/time.py
+++ app/helpers/time.py
@@ -2,10 +2,19 @@ from datetime import datetime, timezone
 
 
 def utc_now_str():
+    """
+    https://momentjs.com/docs/#/displaying/as-iso-string/
+    "2018-01-04T22:44:30.652Z"
+    """
     return ensure_ts_str_ends_with_z(datetime.now(timezone.utc).isoformat())
 
 
 def ensure_ts_str_ends_with_z(ts_string: str):
+    """
+    ensure a iso8601 str is in this format https://momentjs.com/docs/#/displaying/as-iso-string/
+    :param iso8601 ts_string:
+    :return: iso8601 with enforced Z at the end
+    """
     suffix = '+00:00'
     if ts_string.endswith(suffix):
         return ts_string.replace(suffix, 'Z')
