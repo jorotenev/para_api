57ac351d63b7ff25c7a9e1bd79514ba0a623444b
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-20 12:13:14 +0200

    refactor the methods that make Response objects

--- app/api_utils/response.py
+++ app/api_utils/response.py
@@ -4,7 +4,13 @@ from flask import Response
 
 
 def make_json_response(json_str, status_code=200, mimetype='application/json'):
-    if type(json_str) != str:
+    if isinstance(json_str, (dict, list)):
         json_str = json.dumps(json_str)
+    elif not isinstance(json_str, str):
+        raise ValueError("only strings, dicts and lists are accepted")
 
     return Response(json_str, status=status_code, mimetype=mimetype)
+
+
+def make_error_response(msg, status_code=400):
+    return make_json_response({'error': msg}, status_code=status_code)
