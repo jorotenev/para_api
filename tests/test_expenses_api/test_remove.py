f9eff7d45046dc46e617990d8e78d70a3405187c
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-20 13:39:52 +0200

    add a test for the remove() of the expenses_api

--- tests/test_expenses_api/test_remove.py
+++ tests/test_expenses_api/test_remove.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from app.db_facade.facade import NoExpenseWithThisId
 from app.expenses_api.views import db_facade
 from app.expenses_api.views import ApiError
+from app.models.sample_expenses import sample_expenses
 from tests.base_test import BaseTest, BaseTestWithHTTPMethodsMixin
 from tests.test_expenses_api import db_facade_path
 
@@ -13,7 +14,7 @@ endpoint = 'expenses_api.remove'
 class TestRemove(BaseTest, BaseTestWithHTTPMethodsMixin):
 
     def test_normal_usage(self, mocked_db):
-        mocked_db.remove.return_value = True
+        mocked_db.remove.return_value = None
 
         raw_resp = self.delete(url=endpoint, url_for_args={'expense_id': 1})
         self.assertEqual(200, raw_resp.status_code)
@@ -23,11 +24,23 @@ class TestRemove(BaseTest, BaseTestWithHTTPMethodsMixin):
 
     def test_404_on_non_existing_expense(self, mocked_db):
         mocked_db.remove.side_effect = NoExpenseWithThisId()
-
-        raw_resp = self.delete(url=endpoint, url_for_args={"expense_id": 10000})
+        to_delete = sample_expenses[0].copy()
+        raw_resp = self.delete(url=endpoint, data=to_delete)
         self.assertEqual(404, raw_resp.status_code)
 
         self.assertIn(ApiError.NO_EXPENSE_WITH_THIS_ID, raw_resp.get_data(as_text=True))
+
+    def test_400_on_invalid_expense(self, mocked_db):
+        mocked_db.remove.side_effect = RuntimeError("Shouldn't be called")
+        invalid_expenses = []
+
+        invalid_expenses.push({**sample_expenses[0], 'timestamp_utc': ''})
+        invalid_expenses.push({**sample_expenses[0], 'id': ''})
+        for invalid_expense in invalid_expenses:
+            raw_resp = self.delete(url=endpoint, data=invalid_expense)
+            self.assertEqual(400, raw_resp.status_code)
+            self.assertFalse(mocked_db.remove.called)
+            mocked_db.remove.calls.reset_mock()
 
 
 @patch(db_facade_path, autospec=True)
