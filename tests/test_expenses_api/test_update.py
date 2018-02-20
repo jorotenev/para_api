0eab73e36c7a98239b68a82f4f804a6474652d5c
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-20 12:31:38 +0200

    refine the tests for the expenses_api's update()

--- tests/test_expenses_api/test_update.py
+++ tests/test_expenses_api/test_update.py
@@ -33,16 +33,28 @@ class TestUpdate(BaseTestWithHTTPMethodsMixin, BaseTest):
         self.assertDictEqual(updated, exp_json)
 
     def test_400_if_previous_state_not_send(self, mocked_facade):
-        data = {'updated': SINGLE_EXPENSE.copy(), 'previous_state': SINGLE_EXPENSE.copy()}
+        data = {'updated': SINGLE_EXPENSE.copy()}
 
-        json_str = json.dumps(data)
-        raw_resp = self.put(url=endpoint, data=json_str)
+        raw_resp = self.put(url=endpoint, data=data)
         self.assertEqual(400, raw_resp.status_code)
-        self.assertIn(ApiError.PREVIOUS_STATE_OF_EXP_MISSING, raw_resp.get_data(as_text=True)['error'])
+        self.assertIn(ApiError.PREVIOUS_STATE_OF_EXP_MISSING, raw_resp.get_data(as_text=True))
 
     def test_404_on_non_existing_expense(self, mocked_facade):
         mocked_facade.update.side_effect = NoExpenseWithThisId()
 
-        raw_resp = self.put(url=endpoint, data=SINGLE_EXPENSE)
+        raw_resp = self.put(url=endpoint, data={
+            'updated': SINGLE_EXPENSE,
+            'previous_state': SINGLE_EXPENSE
+        })
         self.assertEqual(404, raw_resp.status_code, "Should have returned a 404 for a non-managed expense")
         self.assertIn(ApiError.NO_EXPENSE_WITH_THIS_ID, raw_resp.get_data(as_text=True))
+
+    def test_400_if_ids_dont_match(self,_):
+        data = {
+            'updated': {**SINGLE_EXPENSE},
+            'previous_state': {**SINGLE_EXPENSE, 'id': 'musaka'}
+        }
+
+        raw_resp = self.put(url=endpoint, data=data)
+        self.assertEqual(400, raw_resp.status_code)
+        self.assertIn(ApiError.IDS_OF_EXPENSES_DONT_MATCH, raw_resp.get_data(as_text=True))
