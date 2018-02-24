a08e0b0acc6ba2854d7e79133dd2a7e3c4f287d9
Georgi Tenev <georgetenevbg@gmail.com>
2018-02-24 18:29:19 +0200

    update manage.py so that direct invocation of functions via `zappa invoke` are possible (essentially it seems that methods decorated with @app.cli can't run on lambda when `invoke`d)

--- manage.py
+++ manage.py
@@ -18,6 +18,7 @@ from config import EnvironmentName
 app_mode = None
 try:
     app_mode = os.environ['APP_STAGE']
+    print('app mode is %s' % app_mode)
 except KeyError:
     print("Set the APP_STAGE environmental variable: %s" % ",".join(EnvironmentName.all_names()))
     exit(1)
@@ -34,82 +35,99 @@ def test():
     sys.exit(not result.wasSuccessful())
 
 
-@app.cli.command()
+@app.cli.command(with_appcontext=False)
 def create_expenses_table():
-    from app.db_facade import db_facade
-
-    table_name = db_facade.EXPENSES_TABLE_NAME
-    from app.db_facade.dynamodb.dynamo import create_table_sync
-    from app.db_facade.table_schema import dynamodb_users_table_init_information
-    print("creating dynamodb table [%s]" % table_name)
-    create_table_sync(dynamodb_resource=db_facade.raw_db,
-                      table_name=table_name,
-                      **dynamodb_users_table_init_information)
-    print("ok")
+    _create_expenses_table_no_ctx()
 
 
-@app.cli.command()
-def delete_expenses_table():
-    from app.db_facade import db_facade
-    table_name = db_facade.EXPENSES_TABLE_NAME
-    print("deleting dynamodb table [%s]" % table_name)
+def _create_expenses_table_no_ctx():
+    with app.app_context():
+        from app.db_facade import db_facade
 
-    from app.db_facade.dynamodb.dynamo import DELETE_table_sync
-    DELETE_table_sync(dynamodb_resource=db_facade.raw_db, table_name=table_name)
-    print('ok')
-
-
-@app.cli.command(with_appcontext=True)
-def seed_data():
-    import uuid, datetime
-    from app.db_facade.facade import db_facade
-    from flask import current_app
-    seed = {
-        "id": "",
-        "name": "",
-        "amount": 95,
-        "currency": "EUR",
-        "tags": [
-            "vacation",
-            "vacation",
-            "work"
-        ],
-        "timestamp_utc": "",
-        "timestamp_utc_created": "",
-        "timestamp_utc_updated": ""
-    }
-    now = datetime.datetime.now(datetime.timezone.utc)
-    expenses = []
-    print("seeding dynamodb table [%s]" % db_facade.EXPENSES_TABLE_NAME)
-
-    for i in range(1, 26):
-        temp = seed.copy()
-        temp['user_uid'] = current_app.config['TEST_FIREBASE_UID']
-        temp['id'] = str(uuid.uuid4())
-        temp['name'] = 'server id ' + str(i)
-        temp['timestamp_utc'] = temp['timestamp_utc_created'] = temp[
-            'timestamp_utc_updated'] = ensure_ts_str_ends_with_z((now + datetime.timedelta(seconds=i)).isoformat())
-        expenses.append(temp)
-
-    items = [{"PutRequest": {'Item': expense}} for expense in expenses]
-    unprocessed_items = None
-
-    while unprocessed_items == None or len(unprocessed_items) > 0:
-        resp = db_facade.raw_db.batch_write_item(
-            RequestItems={
-                db_facade.EXPENSES_TABLE_NAME: items
-            }
-        )
-        if resp['UnprocessedItems']:
-            unprocessed_items = resp['UnprocessedItems'][db_facade.EXPENSES_TABLE_NAME]
-        else:
-            break  # no unprocessed items -> done
-    print('ok')
+        table_name = db_facade.EXPENSES_TABLE_NAME
+        from app.db_facade.dynamodb.dynamo import create_table_sync
+        from app.db_facade.table_schema import dynamodb_users_table_init_information
+        print("creating dynamodb table [%s]" % table_name)
+        create_table_sync(dynamodb_resource=db_facade.raw_db,
+                          table_name=table_name,
+                          **dynamodb_users_table_init_information)
+        print("ok")
 
 
 @app.cli.command(with_appcontext=False)
-def boom():
-    app = create_app('staging')
+def delete_expenses_table():
+    _delete_expenses_table_no_ctx()
+
+
+def _delete_expenses_table_no_ctx():
     with app.app_context():
         from app.db_facade import db_facade
-        print(db_facade.EXPENSES_TABLE_NAME)
+        table_name = db_facade.EXPENSES_TABLE_NAME
+        print("deleting dynamodb table [%s]" % table_name)
+
+        from app.db_facade.dynamodb.dynamo import DELETE_table_sync
+        DELETE_table_sync(dynamodb_resource=db_facade.raw_db, table_name=table_name)
+        print('ok')
+
+
+@app.cli.command(with_appcontext=False)
+def seed_data():
+    _seed_data_no_ctx()
+
+
+def _seed_data_no_ctx():
+    with app.app_context():
+        import uuid, datetime
+        from app.db_facade.facade import db_facade
+        from flask import current_app
+        seed = {
+            "id": "",
+            "name": "",
+            "amount": 95,
+            "currency": "EUR",
+            "tags": [
+                "vacation",
+                "vacation",
+                "work"
+            ],
+            "timestamp_utc": "",
+            "timestamp_utc_created": "",
+            "timestamp_utc_updated": ""
+        }
+        now = datetime.datetime.now(datetime.timezone.utc)
+        expenses = []
+        print("seeding dynamodb table [%s]" % db_facade.EXPENSES_TABLE_NAME)
+
+        for i in range(1, 26):
+            temp = seed.copy()
+            temp['user_uid'] = current_app.config['TEST_FIREBASE_UID']
+            temp['id'] = str(uuid.uuid4())
+            temp['name'] = 'server id ' + str(i)
+            temp['timestamp_utc'] = temp['timestamp_utc_created'] = temp[
+                'timestamp_utc_updated'] = ensure_ts_str_ends_with_z((now + datetime.timedelta(seconds=i)).isoformat())
+            expenses.append(temp)
+
+        items = [{"PutRequest": {'Item': expense}} for expense in expenses]
+        unprocessed_items = None
+
+        while unprocessed_items == None or len(unprocessed_items) > 0:
+            resp = db_facade.raw_db.batch_write_item(
+                RequestItems={
+                    db_facade.EXPENSES_TABLE_NAME: items
+                }
+            )
+            if resp['UnprocessedItems']:
+                unprocessed_items = resp['UnprocessedItems'][db_facade.EXPENSES_TABLE_NAME]
+            else:
+                break  # no unprocessed items -> done
+        print('ok')
+
+
+@app.cli.command()
+def boom():
+    print("command ran in %s" % app.config['APP_STAGE'])
+
+
+def bau():
+    with app.app_context():
+        print(app.config['APP_STAGE'])
